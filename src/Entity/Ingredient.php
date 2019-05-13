72e06f50bc06bbd3522ebec98b07e4ac8cee7fe4
iponzsolti <zsolti@ipon.hu>
2019-05-13 10:23:16 +0200

    Set entity database table names

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\IngredientRepository")
+ * @ORM\Table(name="ingredients")
  */
 class Ingredient
 {
--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RecipeRepository")
+ * @ORM\Table(name="recipes")
  */
 class Recipe
 {
